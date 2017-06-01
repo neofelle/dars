e38245f09fd06472b19507972e77a2689cc2403e
Marlito Dungog <marlito.dungog@gmail.com>
2017-05-31 18:57:32 -0700

    Setup pages and plugins

--- wp-content/themes/custom-theme/single.php
+++ wp-content/themes/custom-theme/single.php
@@ -1,24 +1,23 @@
 <?php get_header('inner'); ?>
-<br class="clear" /><br/> 
 <section class="page-section">
-    <div class="container page-content">
-        <h1><u class="page"><?php the_title();?></u></h1>
-        <br>
-        <div class="col-md-12">
-            <?php
-                while ( have_posts() ) : the_post();
+    <div class="col-md-12" style="background-color: white;">
+        <div class="container page-content">
+            <h1><?php the_title();?></h1>
+            <br>
+            <div class="col-md-12">
+                <?php
+                    while ( have_posts() ) : the_post();
 
-                    get_template_part( 'template-parts/page/content', 'page' );
-                    the_content();
-                    // If comments are open or we have at least one comment, load up the comment template.
-                    if ( comments_open() || get_comments_number() ) :
-                        comments_template();
-                    endif;
+                        get_template_part( 'template-parts/page/content', 'page' );
+                        the_content();
+                        // If comments are open or we have at least one comment, load up the comment template.
+                        if ( comments_open() || get_comments_number() ) :
+                            comments_template();
+                        endif;
 
-                endwhile; // End of the loop.
-
-
-            ?>
+                    endwhile; // End of the loop.
+                ?>
+            </div>
         </div>
     </div>
  </section>
